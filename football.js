fbdd9cb151b0206ea7d843e068ca3828c1ccba1d
jcd1141 <joshd3887@gmail.com>
2026-09-14 14:20:34 +0000

    debugged football, added drive log

--- football.js
+++ football.js
@@ -6,11 +6,11 @@
 let teamAScore = 0;
 let teamBScore = 0;
 
-let drivesA = 10;
-let drivesB = 10;
+let drivesA = 6;
+let drivesB = 6;
+let currentDrive = 1;
 
 while(drivesA!= 0 && drivesB!=0){
-    let currentDrive = 1;
 
     let outcome = Math.floor(Math.random() * 3) + 1;
 
@@ -20,23 +20,26 @@ while(drivesA!= 0 && drivesB!=0){
     }
     else if(outcome == 2){
         teamAScore += 3;
-        console.log("Team A Drive " + currentDrive + ":" + "Scored a field goal");
+        console.log("Team A Drive " + currentDrive + ":" + " Scored a field goal");
     }
     else{
         teamAScore += 0;
-        console.log("Team A Drive " + currentDrive + ":" + "Punted");
+        console.log("Team A Drive " + currentDrive + ":" + " Punted");
     }
     drivesA--;
 
     let outcomeB = Math.floor(Math.random() * 3) + 1;
     if(outcomeB == 1){
         teamBScore += 7;
+        console.log("Team B Drive " + currentDrive + ":" + " Scored a touchdown");
     }
     else if(outcomeB == 2){
         teamBScore += 3;
+        console.log("Team B Drive " + currentDrive + ":" + " Scored a field goal");
     }
     else{
         teamBScore += 0;
+        console.log("Team B Drive " + currentDrive + ":" + " Punted");
     }
     drivesB--;
 
